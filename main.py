11cc4253b9825f41e5e9c35660bf46ae5546930d
3a4em!? <zateevvadim7@gmail.com>
2026-06-11 17:00:41 +0700

    class GameSprit added

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ bg_c = (128, 128, 128)
 timer = time.Clock()
 game = True
 
+# --- Класс GameSprit
+class GameSprite(sprite.Sprite):
+    def __init__(self, x,y , widih, height, image_file):
+            
+            self.image = transform.scale(image.load(image_file), (widih, height))
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+                
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
