8d7d7c681355605b0087eba41e39cb6b90acc1f5
3a4em!? <zateevvadim7@gmail.com>
2026-06-15 14:57:45 +0700

    add platform group

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ game = True
 
 # --- Класс GameSprit
 class GameSprite(sprite.Sprite):
-    def __init__(self, x,y , widih, height, image_file):
-            
-            self.image = transform.scale(image.load(image_file), (widih, height))
-            self.rect = self.image.get_rect()
-            self.rect.x = x
-            self.rect.y = y
+    def __init__(self, x,y , widih, height, image_file,*group):
+        super().__init__(*group)    
+        self.image = transform.scale(image.load(image_file), (widih, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
                 
@@ -26,7 +26,7 @@ class GameSprite(sprite.Sprite):
 class Platform(GameSprite):
     def __init__(self, x, y, widih, height, image_file, speed, keyU, keyD):
         super().__init__(x, y, widih, height, image_file)
-        
+
         self.speed = speed
         self.keyU = keyU
         self.keyD = keyD
@@ -40,11 +40,19 @@ class Platform(GameSprite):
         if keys[self.keyD] and self.rect.y < 800 - self.rect.height:
             self.rect.y += self.speed
 
+platform_left = Platform(10, 300 , 40, 200, "racket.png", 5, K_w, K_s)
+platform_right = Platform(750, 300 , 40, 200, "racket.png", 5, K_UP, K_DOWN)
+platforms = sprite.Group()
+
+platforms.add(platform_left,platform_right)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     window.fill(bg_c)
+    platforms.draw(window)
+    platforms.update()
 
     display.update()
     timer.tick(40)
